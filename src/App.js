1975e7e3d9a300c1489e0734e2c58ad53bf192d1
Ethan Fang <ethan.fang@gmail.com>
2023-02-14 13:13:32 +1100

    Calculate winner and restart game has been added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import {StrictMode, useState} from "react";
-import {createRoot} from "react-dom/client";
+import {useState} from "react";
 import './App.css';
 
 function Square({value, onSquareClick}){
@@ -9,13 +8,45 @@ function Square({value, onSquareClick}){
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default function Board(){
   // first index is the current state
   // second index is the function to let you update it
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(index) {
+    if (squares[index] || calculateWinner(squares)) {
+      return
+    }
+
     const nextSquares = squares.slice();
     if(xIsNext) {
       nextSquares[index] = "X";
@@ -27,11 +58,22 @@ export default function Board(){
     setXIsNext(!xIsNext)
   }
 
+  function resetButton() {
+    for(let index = 0; index < squares.length; index++) {
+      squares[index] = null
+    }
+    setSquares(squares)
+    setXIsNext("X")
+    return
+  }
+
   return (
-    // fragments are used ti let you group a list of children without adding extra nodes to the dom
+    // fragments are used to let you group a list of children without adding extra nodes to the dom
     <>
+      <h1>Tic Tac Toe</h1>
+      <div className="status">{status}</div>
       <div className="board-row">
-        {/* anrrow function is used to negate handleClick() from being called immediately */}
+        {/* an arrow function is used to negate handleClick() from being called immediately */}
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
@@ -45,6 +87,10 @@ export default function Board(){
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+      <br/>
+      <div>
+        <button onClick={resetButton}>Reset</button>
       </div>
     </>
   );
